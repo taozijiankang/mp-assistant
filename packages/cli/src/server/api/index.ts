028e9c598221727298cff9f3627da532103ef312
YAYALUOYA <yayaluoya@sina.com>
2026-02-26 16:03:45 +0800

    feat: 上传文件的功能

--- packages/cli/src/server/api/index.ts
+++ packages/cli/src/server/api/index.ts
@@ -1,8 +1,10 @@
 import { FastifyInstance } from "fastify";
 import { registerConfigApi } from "./modules/config.js";
 import { registerWorkerApi } from "./modules/worker.js";
+import { registerCommonApi } from "./modules/common.js";
 
 export const registerApi = (fastify: FastifyInstance) => {
+    registerCommonApi(fastify);
     registerConfigApi(fastify);
     registerWorkerApi(fastify);
 }
