04b7c7442d7022124ed34a5a40a7f5665b222ada
YAYALUOYA <yayaluoya@sina.com>
2026-02-28 15:24:36 +0800

    fix: -

--- packages/dashboard/src/component/WorkerDetail/component/TaskStack/index.vue
+++ packages/dashboard/src/component/WorkerDetail/component/TaskStack/index.vue
@@ -57,9 +57,10 @@
                                 <div class="wxa-username">username: {{ getWxaInfo(taskItem.options)?.username }}</div>
                             </div>
                         </div>
-                        <div v-if="WXTaskN.isPublishInfo(taskItem)" class="publish-qrcode-container">
+                        <div v-if="WXTaskN.isPublishInfo(taskItem) && taskItem.publishQRCodeFilePath"
+                            class="publish-qrcode-container">
                             <span class="publish-qrcode-description">需要扫描二维码进行发布 剩余时间: {{ Math.round(taskItem.countdown)
-                                }}秒</span>
+                            }}秒</span>
                             <img :src="getFileUrl(taskItem.publishQRCodeFilePath)" alt="publish-qrcode"
                                 class="publish-qrcode-image" />
                         </div>
