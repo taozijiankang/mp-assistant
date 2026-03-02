a1755523ff5b91043a82d52bed60db56ba71cce5
YAYALUOYA <yayaluoya@sina.com>
2026-03-02 11:25:04 +0800

    feat: -

--- packages/cli/src/bin/index.ts
+++ packages/cli/src/bin/index.ts
@@ -8,19 +8,30 @@ import { start } from '../start.js';
 
 const rootPackageJson = JSON.parse(fs.readFileSync(path.join(getRootDir(), 'package.json')).toString());
 
+interface StartCommandOptions {
+    executablePath: string;
+    headless: boolean;
+    port: number;
+}
+
 program
     .version(rootPackageJson.version)
     .description('小程序助手')
     // 
     .command('start')
-    .action(() => {
+    .option('-e, --executablePath <executablePath>', '浏览器可执行文件路径')
+    .option('-h, --headless <headless>', '是否无头模式')
+    .option('-p, --port <port>', '端口号 (默认: 3001)')
+    .action(async (options: StartCommandOptions) => {
+        const { executablePath, headless, port } = options;
+
         ConfigStore.instance.setConfig({
-            executablePath: 'C:/Program Files/Google/Chrome/Application/chrome.exe',
-            headless: false,
-            port: 3001,
+            executablePath: executablePath || ConfigStore.instance.config.executablePath,
+            headless: headless || ConfigStore.instance.config.headless,
+            port: port || ConfigStore.instance.config.port,
         });
 
-        start();
+        await start();
     });
 
 program.parse(process.argv);
